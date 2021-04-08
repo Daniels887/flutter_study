a27fddaa11934de5b2c5b16560ae8f008a5e005e
Daniel <daniel_sousa.p@outlook.com>
2021-04-07 23:53:47 -0300

    refactor: move widgets to own file

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
+import 'app_widget.dart';
+
 void main() {
-  runApp(AppWidget(title: 'Daniel',));
-}
-
-class AppWidget extends StatelessWidget {
-
-  final String title;
-
-  const AppWidget({Key key, this.title}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      child: Center(
-        child: Text(
-          title, 
-          textDirection: TextDirection.ltr,
-          style: TextStyle(color: Colors.white, 
-          fontSize: 50.0)
-        ),
-      ),
-    );
-  }
+  runApp(AppWidget());
 }
