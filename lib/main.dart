e6c54096946b69c5803194480c1b9c6985278002
Daniel <daniel_sousa.p@outlook.com>
2021-04-07 23:27:50 -0300

    First commit

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+void main() {
+  runApp(AppWidget(title: 'Daniel',));
+}
+
+class AppWidget extends StatelessWidget {
+
+  final String title;
+
+  const AppWidget({Key key, this.title}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: Text(
+          title, 
+          textDirection: TextDirection.ltr,
+          style: TextStyle(color: Colors.white, 
+          fontSize: 50.0)
+        ),
+      ),
+    );
+  }
+}
