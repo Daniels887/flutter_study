b0121e320ff3fe316196028a145d74c0409cf834
Daniel <daniel_sousa.p@outlook.com>
2021-04-11 21:31:47 -0300

    feat: add image

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -26,6 +26,14 @@ class _LoginPageState extends State<LoginPage> {
               child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
+                Container(
+                  width: 150,
+                  height: 150,
+                  child: Image.network(
+                    'https://upload.wikimedia.org/wikipedia/commons/thumb/a/ab/Logo_TV_2015.png/250px-Logo_TV_2015.png'
+                  ),
+                ),
+                Container(height: 16),
                 TextField(
                   keyboardType: TextInputType.emailAddress,
                   onChanged: (text) {
